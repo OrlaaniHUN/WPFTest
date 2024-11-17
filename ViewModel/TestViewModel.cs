267a0c02ee4e132c0c93f8eaa5b896f0ee3b77bb
OrlaaniHUN <orlaani@protonmail.com>
2024-11-17 15:05:12 +0100

    add fiels

---
+++ ViewModel/TestViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace ViewModel
+{
+    public class TestviewModel : ViewModelBase
+    {
+        private string _text = "Hello World!";
+        public string Text
+        {
+            get 
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
